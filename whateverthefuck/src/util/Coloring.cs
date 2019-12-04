352ad110929e87015fafa91b4b79c887edc68288
Jasin Ali <alijasin93@gmail.com>
2019-12-04 02:11:04 +0100

    added coloring util class

---
+++ whateverthefuck/src/util/Coloring.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whateverthefuck.src.util
+{
+    static class Coloring
+    {
+        static Random r = new Random(3434);
+        public static Color RandomColor()
+        {
+            return Color.FromArgb(255, r.Next(255), r.Next(255), r.Next(255));
+        }
+
+        public static Color Opposite(Color c)
+        {
+            return Color.FromArgb(c.ToArgb() ^ 0xffffff);
+        }
+    }
+}
