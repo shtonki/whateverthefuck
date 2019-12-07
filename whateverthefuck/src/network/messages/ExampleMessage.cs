b3b536ece9ea52a86bb185ad5249ff8a46a2ffd6
seba <sebastiandeverdier@gmail.com>
2019-12-07 13:14:56 -0500

    pre overhaul commit. jesus take the wheel.

--- whateverthefuck/src/network/messages/ExampleMessage.cs
+++ whateverthefuck/src/network/messages/ExampleMessage.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,15 +26,20 @@ namespace whateverthefuck.src.network.messages
         }
     }
 
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
     struct ExampleBody : MessageBody
     {
         int a;
         float b;
 
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 5)]
+        string bs;
+
         public ExampleBody(int a, float b)
         {
             this.a = a;
             this.b = b;
+            bs = "walla";
         }
     }
 }
